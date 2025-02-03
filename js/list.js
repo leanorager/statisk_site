ba6db0a69385f77baa001b64d50f95ea29beed54
Lea Nørager <leno0001@stud.kea.dk>
2025-02-03 12:38:29 +0100

    rettelse af list.js

--- js/list.js
+++ js/list.js
@@ -1,15 +1,14 @@
 const productList = document.querySelector(".product_list_container");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=100`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
 function showList(products) {
   console.log(products);
-  let markup = "";
-  products
-    .map((product) => {
-      markup += `<a href="product.html">
+  const markup = products
+    .map(
+      (product) => `<a href="product.html">
           <div class="product_card discount1">
             <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="Produkt" />
             <div class="product_info">
@@ -21,8 +20,8 @@ function showList(products) {
               <span class="new_price">DKK ${product.price}</span>
             </div>
           </div>
-        </a>`;
-    })
+        </a>`
+    )
     .join("");
   console.log(markup);
   productList.innerHTML = markup;
